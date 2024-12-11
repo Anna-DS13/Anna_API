bd56715a27bbeadcea56042cd8426b478a4c43c1
Anna-DS13 <annascariakp138@gmail.com>
2024-12-11 11:13:44 +0530

    prediction api started

--- api/main.py
+++ api/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from prediction import get_city_map, predict_price
 
 app = FastAPI()
 
 
-@app.get("/index")
-def index():
-    return {"name":"Anna","designation":"Student"}
+@app.get("/api/predict")
+def predict(size,total_sqft,bath,balcony,location):
+    price = list(predict_price(size,total_sqft,bath,balcony,location))[0]
+    return {"message":"Success","Data":{"predict_price":price},"ErrorCode":0}
+
+@app.get("/api/citylist")
+def get_cit_map_api():
+    city_map = get_city_map()
+    return {"message":"Success","Data":city_map,"ErrorCode":0}
